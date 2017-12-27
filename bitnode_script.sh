ff64c3d8e777a22881028a769f4afd7cb46ebde0
mrkss21 <34880782+mrkss21@users.noreply.github.com>
2017-12-27 14:26:39 +0300

    Update bitnode_script.sh

--- bitnode_script.sh
+++ bitnode_script.sh
@@ -1,5 +1,5 @@
 sudo apt -y update
-udo apt -y update
+sudo apt -y update
 sudo apt -y install build-essential libssl-dev libdb++-dev && sudo apt -y install libboost-all-dev libcrypto++-dev libqrencode-dev && sudo apt -y install libminiupnpc-dev libgmp-dev libgmp3-dev autoconf && sudo apt -y install autogen automake libtool autotools-dev pkg-config && sudo apt -y install bsdmainutils software-properties-common && sudo apt -y install libzmq3-dev libminiupnpc-dev libssl-dev libevent-dev && sudo add-apt-repository ppa:bitcoin/bitcoin
 sudo apt-get update
 sudo apt-get install libdb4.8-dev libdb4.8++-dev -y
